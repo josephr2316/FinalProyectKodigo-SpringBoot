c3228e4023c0b245446d4d18eb17c8581a2fb76c
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-11-22 10:26:16 -0400

    The docker dependency was commented

---
+++ fpshoppingcart/src/main/java/com/lunifer/jo/fpshoppingcart/entity/Product.java
@@ -0,0 +1,36 @@
+package com.lunifer.jo.fpshoppingcart.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable=false)
+    private long productId;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private int stock;
+
+    @Column(columnDefinition = "boolean default true")
+    private boolean isActive;
+
+}
