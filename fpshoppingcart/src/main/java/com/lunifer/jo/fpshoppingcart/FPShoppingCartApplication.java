15f17973683307e41ac6d7e2b8cbb48496d118dc
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-11-22 04:03:57 -0400

    Refactoring the main class

--- fpshoppingcart/src/main/java/com/lunifer/jo/fpshoppingcart/FPShoppingCartApplication.java
+++ fpshoppingcart/src/main/java/com/lunifer/jo/fpshoppingcart/FPShoppingCartApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FpshoppingcartApplication {
+public class FPShoppingCartApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FpshoppingcartApplication.class, args);
+		SpringApplication.run(FPShoppingCartApplication.class, args);
 	}
 
 }
