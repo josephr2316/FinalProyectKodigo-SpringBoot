e2512dec4204affcd5e2cdcccc271c6fe4e08fb7
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-11-22 02:50:38 -0400

    Creation final project kodigo shopping cart

---
+++ fpshoppingcart/src/main/java/com/lunifer/jo/fpshoppingcart/FpshoppingcartApplication.java
@@ -0,0 +1,13 @@
+package com.lunifer.jo.fpshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpshoppingcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpshoppingcartApplication.class, args);
+	}
+
+}
