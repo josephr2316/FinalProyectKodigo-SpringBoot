51270e40d9418277f702cae8e5c0ba5765ab8eae
Nico Vitti <nicovitti@hotmail.com>
2023-11-24 11:27:30 -0300

    ShoppingCartService added

---
+++ fpshoppingcart/src/main/java/com/lunifer/jo/fpshoppingcart/service/ShoppingCartService.java
@@ -0,0 +1,18 @@
+package com.lunifer.jo.fpshoppingcart.service;
+
+import com.lunifer.jo.fpshoppingcart.dto.ShoppingCartDTO;
+
+import java.util.List;
+
+public interface ShoppingCartService {
+
+    ShoppingCartDTO getShoppingCartById(Long cartId);
+
+    ShoppingCartDTO createShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    ShoppingCartDTO updateShoppingCart(Long cartId, ShoppingCartDTO shoppingCartDTO);
+
+    void deleteShoppingCart(Long cartId);
+
+    List<ShoppingCartDTO> getAllShoppingCarts();
+}
