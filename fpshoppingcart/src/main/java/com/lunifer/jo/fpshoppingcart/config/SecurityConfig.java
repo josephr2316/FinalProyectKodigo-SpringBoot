54bdd109ba32872c9ddb84c0583e2d069aa5f0de
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-11-26 16:47:56 -0400

    Finishing SecurityConfig

--- fpshoppingcart/src/main/java/com/lunifer/jo/fpshoppingcart/config/SecurityConfig.java
+++ fpshoppingcart/src/main/java/com/lunifer/jo/fpshoppingcart/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.lunifer.jo.fpshoppingcart.config;
 
 import com.lunifer.jo.fpshoppingcart.service.UserService;
 import io.jsonwebtoken.Jwt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,17 +11,23 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.PasswordManagementDsl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-    private UserService userService;
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final UserService userService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
+    @Autowired
     public SecurityConfig(UserService userService, JwtAuthenticationFilter jwtAuthenticationFilter){
         this.userService = userService;
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
@@ -37,6 +44,18 @@ public class SecurityConfig {
         authenticationProvider.setUserDetailsService(userService);
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChainGeneral(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests( authorization -> authorization
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/")).permitAll()
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/auth/**")).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .build();
     }
 
     @Bean
