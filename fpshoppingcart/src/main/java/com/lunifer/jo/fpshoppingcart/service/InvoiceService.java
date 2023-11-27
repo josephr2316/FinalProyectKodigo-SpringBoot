41f56e71033b839eed7fe6fa75be856504c1bc17
Carvajal Patiño Luis Enrique <44406616+carvajalluisweb@users.noreply.github.com>
2023-11-27 16:10:26 -0500

    Update InvoiceService.java

--- fpshoppingcart/src/main/java/com/lunifer/jo/fpshoppingcart/service/InvoiceService.java
+++ fpshoppingcart/src/main/java/com/lunifer/jo/fpshoppingcart/service/InvoiceService.java
@@ -1,7 +1,7 @@
 package com.lunifer.jo.fpshoppingcart.service;
 
 import com.lunifer.jo.fpshoppingcart.dto.InvoiceDTO;
-import com.lunifer.jo.fpshoppingcart.entity.Invoice;
+
 
 public interface InvoiceService {
     public InvoiceDTO createInvoice(InvoiceDTO invoiceDTO);
