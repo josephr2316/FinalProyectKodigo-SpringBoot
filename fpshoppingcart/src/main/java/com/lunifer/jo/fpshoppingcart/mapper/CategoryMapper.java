63479c970ccef55f087839f1cedeb83b61600420
Jose Miguel Rodriguez Kelly <josephr2316@gmail.com>
2025-07-14 00:16:32 -0400

    Refactor Category Mapper

--- fpshoppingcart/src/main/java/com/lunifer/jo/fpshoppingcart/mapper/CategoryMapper.java
+++ fpshoppingcart/src/main/java/com/lunifer/jo/fpshoppingcart/mapper/CategoryMapper.java
@@ -1,14 +1,18 @@
 package com.lunifer.jo.fpshoppingcart.mapper;
 
-import com.lunifer.jo.fpshoppingcart.dto.CategoryDTO;
+import com.lunifer.jo.fpshoppingcart.dto.*;
 import com.lunifer.jo.fpshoppingcart.entity.Category;
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
-    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
-    CategoryDTO categoryEntityToCategoryDTO(Category category);
-    Category categoryDTOToCategoryEntity(CategoryDTO categoryDTO);
 
+    CategoryDTO toCategoryDTO(Category category);
+
+    @Mapping(target = "categoryId", ignore = true)
+    @Mapping(target = "products", ignore = true)
+    Category toCategory(CreateCategoryDTO dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateCategoryFromDTO(UpdateCategoryDTO dto, @MappingTarget Category category);
 }
