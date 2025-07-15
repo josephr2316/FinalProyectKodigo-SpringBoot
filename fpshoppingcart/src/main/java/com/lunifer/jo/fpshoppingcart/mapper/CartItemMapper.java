332c12c2081bdcc9d4ff4430746d2f28a22b537d
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2025-07-15 11:44:29 -0400

    Refactor CartItemMapper

---
+++ fpshoppingcart/src/main/java/com/lunifer/jo/fpshoppingcart/mapper/CartItemMapper.java
@@ -0,0 +1,15 @@
+package com.lunifer.jo.fpshoppingcart.mapper;
+
+import com.lunifer.jo.fpshoppingcart.dto.*;
+import com.lunifer.jo.fpshoppingcart.entity.CartItem;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface CartItemMapper {
+
+    @Mapping(target = "cartId", source = "cart.cartId")
+    @Mapping(target = "productId", source = "product.productId")
+    @Mapping(target = "productName", source = "product.productName")
+    @Mapping(target = "productPrice", source = "product.price")
+    CartItemDTO toCartItemDTO(CartItem cartItem);
+}
