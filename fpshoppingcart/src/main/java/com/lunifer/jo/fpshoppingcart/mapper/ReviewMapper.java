c78468ebae6c5028ec78dd5b04c4cf8518f08520
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2025-07-16 02:49:12 +0000

    Refactor ReviewMapper

--- fpshoppingcart/src/main/java/com/lunifer/jo/fpshoppingcart/mapper/ReviewMapper.java
+++ fpshoppingcart/src/main/java/com/lunifer/jo/fpshoppingcart/mapper/ReviewMapper.java
@@ -1,19 +1,18 @@
 package com.lunifer.jo.fpshoppingcart.mapper;
 
-import com.lunifer.jo.fpshoppingcart.dto.ReviewDTO;
+import com.lunifer.jo.fpshoppingcart.dto.*;
 import com.lunifer.jo.fpshoppingcart.entity.Review;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface ReviewMapper {
 
     @Mapping(target = "productId", source = "product.productId")
+    @Mapping(target = "productName", source = "product.productName")
     @Mapping(target = "userId", source = "user.userId")
-    ReviewDTO reviewEntityToReviewDTO(Review reviewEntity);
-
-    @Mapping(target = "product.productId", source = "productId")
-    @Mapping(target = "user.userId", source = "userId")
-    Review reviewDTOToReviewEntity(ReviewDTO reviewDTO);
-}
+    @Mapping(target = "username", source = "user.username")
+    @Mapping(target = "userFullName", expression = "java(review.getUser().getFullName())")
+    @Mapping(target = "positive", expression = "java(review.isPositive())")
+    @Mapping(target = "negative", expression = "java(review.isNegative())")
+    @Mapping(target = "neutral", expression = "java(review.isNeutral())")
+  
