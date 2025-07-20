d2ecfe281b9c3ea4075ad0233983f990dc0f164a
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2025-07-20 19:45:07 +0000

    Refactor user reposition

--- fpshoppingcart/src/main/java/com/lunifer/jo/fpshoppingcart/repository/UserRepository.java
+++ fpshoppingcart/src/main/java/com/lunifer/jo/fpshoppingcart/repository/UserRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 
-    @Query("SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.shoppingCartHistory sc LEFT JOIN FETCH u.orderHistory o")
+    //@Query("SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.shoppingCartHistory sc LEFT JOIN FETCH u.orderHistory o")
     Page<User> findAll(Pageable pageable);
 
 }
+
